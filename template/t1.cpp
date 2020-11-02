cbc0761f1adaf4d2b08afbf0e839392da80ca9f6
DCYokaze <mote.beats@gmail.com>
2020-11-02 07:26:28 +0700

    add template.

---
+++ template/t1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <math.h>
+// #include <vector>
+#include <map>
+// #include <set>
+using namespace std;
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  return 0;
+}
---
+++ template/t1.py
@@ -0,0 +1,11 @@
+def procVal():
+  return
+
+if __name__ == '__main__':
+  n1 = int(input())
+  for i in range(n1):
+    pAll = list(map(int, input().split()))
+    # do something.
+
+    # print( output )
+  exit()
